30a82f76398e66fe6421a9faa25cf4af949e9667
Swordikk <133685730+Swordikk@users.noreply.github.com>
2024-11-09 17:24:39 +0400

    Update .lua

--- .lua
+++ .lua
@@ -23,31 +23,66 @@ local DiosDiary = PlayerName.Backpack:WaitForChild("Dio's Diary")
 
 -- Function Misc --
 
--- Tab Home --
+---- Tab Home ----
 local Tab = Window:MakeTab({
 	Name = "Home",
 	Icon = "rbxassetid://4370345144",
 	PremiumOnly = false
 })
 
--- Tab Farming --
+---- Tab Farming ----
 local Tab = Window:MakeTab({
 	Name = "Farming",
 	Icon = "rbxassetid://4483364237",
 	PremiumOnly = false
 })
 
-Tab:AddButton({
-	Name = "Script for AutoFarm",
-	Callback = function()
-
-  	end    
-})
-
--- Tab Items --
+---- Tab Items ----
 local Tab = Window:MakeTab({
 	Name = "Items",
+	Icon = "rbxassetid://4335482575",
+	PremiumOnly = false
+})
+
+---- Tab Teleports ----
+local Tab = Window:MakeTab({
+	Name = "Teleports",
 	Icon = "rbxassetid://4384402413",
+	PremiumOnly = false
+})
+
+---- Tab Misc ----
+local Tab = Window:MakeTab({
+	Name = "Misc",
+	Icon = "rbxassetid://4483362748",
+	PremiumOnly = false
+})
+
+---- Tab Themes ----
+local Tab = Window:MakeTab({
+	Name = "Themes",
+	Icon = "rbxassetid://4335483762",
+	PremiumOnly = false
+})
+
+---- Tab Visuals ----
+local Tab = Window:MakeTab({
+	Name = "Visuals",
+	Icon = "rbxassetid://3610254229",
+	PremiumOnly = false
+})
+
+---- Tab Configs ----
+local Tab = Window:MakeTab({
+	Name = "Configs",
+	Icon = "rbxassetid://3610247188",
+	PremiumOnly = false
+})
+
+---- Tab Setthing ----
+local Tab = Window:MakeTab({
+	Name = "Setthings",
+	Icon = "rbxassetid://4483345737",
 	PremiumOnly = false
 })
 
@@ -146,90 +181,5 @@ Tab:AddToggle({
 		
 	end
 })]]
-
--- Tab Teleports --
-local Tab = Window:MakeTab({
-	Name = "Teleports",
-	Icon = "rbxassetid://4384402413",
-	PremiumOnly = false
-})
-
--- Tab Misc --
-local Tab = Window:MakeTab({
-	Name = "Misc",
-	Icon = "rbxassetid://4483362748",
-	PremiumOnly = false
-})
-
-Tab:AddToggle({
-	Name = "Anti-AFK",
-	Default = false,
-	Callback = function(Value)
-		if Value == true then
-			while not game:IsLoaded() do wait() end
-			repeat wait() until game.Players.LocalPlayer.Character
-			Players = game:GetService("Players")
-			local GC = getconnections or get_signal_cons
-			if GC then
-				for i,v in pairs(GC(Players.LocalPlayer.Idled)) do
-					if v["Disable"] then v["Disable"](v)
-					elseif v["Disconnect"] then v["Disconnect"](v)
-					end
-				end
-			else
-			Players.LocalPlayer.Idled:Connect(function()
-				VirtualUser:CaptureController()
-				VirtualUser:ClickButton2(Vector2.new())
-  				end)
-			end
-		end
-	end
-})
-
-Tab:AddTextbox({
-	Name = "WalkSpeed",
-	Default = "",
-	TextDisappear = false,
-	Callback = function(Value)
-		
-	end
-})
-
-Tab:AddTextbox({
-	Name = "JumpPower",
-	Default = "",
-	TextDisappear = false,
-	Callback = function(Value)
-		
-	end
-})
-
--- Tab Themes --
-local Tab = Window:MakeTab({
-	Name = "Themes",
-	Icon = "rbxassetid://4335483762", 
-	PremiumOnly = false
-})
-
--- Tab Visuals --
-local Tab = Window:MakeTab({
-	Name = "Visuals",
-	Icon = "rbxassetid://3610254229",
-	PremiumOnly = false
-})
-
--- Tab Configs --
-local Tab = Window:MakeTab({
-	Name = "Configs",
-	Icon = "rbxassetid://3610247188",
-	PremiumOnly = false
-})
-
--- Tab Setthings --
-local Tab = Window:MakeTab({
-	Name = "Setthings",
-	Icon = "rbxassetid://4483345737",
-	PremiumOnly = false
-})
 
 OrionLib:Init()
