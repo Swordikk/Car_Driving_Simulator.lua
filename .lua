e72c5614bf73cfc38408d22895ee2eedda8c2bfc
Swordikk <133685730+Swordikk@users.noreply.github.com>
2024-11-10 00:05:32 +0400

    Update .lua

--- .lua
+++ .lua
@@ -11,287 +11,73 @@ local function getItem(itemName)
     return Player.Backpack:FindFirstChild(itemName) or Player.Character:FindFirstChild(itemName)
 end
 
-local Rokakaka = getItem("Rokakaka")
-local PureRokakaka = getItem("Pure Rokakaka")
-local Diamond = getItem("Diamond")
-local GoldCoin = getItem("Gold Coin")
-local ZepellinsHeadband = getItem("Zepellin's Headband")
-local MysteriousArrow = getItem("Mysterious Arrow")
-local StoneMask = getItem("Stone Mask")
-local QuintonsGlove = getItem("Quinton's Glove")
-local SteelBall = getItem("SteelBallRunWins")
-local RibCage = getItem("Rib Cage of The Saint's Corpse")
-local AncientScroll = getItem("Ancient Scroll")
-local DiosDiary = getItem("Dio's Diary")
-
--- Function Items
-local function AutoRokakaka()
-    while _G.Rokakaka do wait(0.01)
-        if Rokakaka then
-            Rokakaka.Parent = Character
+local function AutoItem(itemName, callback)
+    while _G[itemName] do wait(0.01)
+        local item = getItem(itemName)
+        if item then
+            item.Parent = Character
             wait(1)
-			local args = {
-				[1] = "EndDialogue",
-				[2] = {
-					["Option"] = "Option2",
-					["NPC"] = "Merchant",
-					["Dialogue"] = "Dialogue5"
-				}
-			}
-			game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(args))
+            local args = {
+                [1] = "EndDialogue",
+                [2] = {
+                    ["Option"] = "Option2",
+                    ["NPC"] = "Merchant",
+                    ["Dialogue"] = "Dialogue5"
+                }
+            }
+            game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(args))
         end
     end
-end
-
-local function AutoGoldCoin()
-	while _G.GoldCoin do wait(0.01)
-		if GoldCoin then
-			GoldCoin.Parent = Character
-			wait(1)
-			local args = {
-				[1] = "EndDialogue",
-				[2] = {
-					["Option"] = "Option2",
-					["NPC"] = "Merchant",
-					["Dialogue"] = "Dialogue5"
-				}
-			}
-			game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(args))
-		end
-	end
-end
-
-local function AutoMystArrow()
-	while _G.MysteriousArrow do wait(0.01)
-		if MysteriousArrow then
-			MysteriousArrow.Parent = Character
-			wait(1)
-			local args = {
-				[1] = "EndDialogue",
-				[2] = {
-					["Option"] = "Option2",
-					["NPC"] = "Merchant",
-					["Dialogue"] = "Dialogue5"
-				}
-			}
-			game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(args))
-		end
-	end
-end
-
-local function AutoDiamond()
-	while _G.Diamond do wait(0.01)
-		if Diamond then
-			Diamond.Parent = Character
-			wait(1)
-			local args = {
-				[1] = "EndDialogue",
-				[2] = {
-					["Option"] = "Option2",
-					["NPC"] = "Merchant",
-					["Dialogue"] = "Dialogue5"
-				}
-			}
-			game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(args))
-		end
-	end
-end
-
-local function AutoPureRokakaka()
-	while _G.PureRokakaka do wait(0.01)
-		if PureRokakaka then
-			PureRokakaka.Parent = Character
-			wait(1)
-			local args = {
-				[1] = "EndDialogue",
-				[2] = {
-					["Option"] = "Option2",
-					["NPC"] = "Merchant",
-					["Dialogue"] = "Dialogue5"
-				}
-			}
-			game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(args))
-		end
-	end
-end
-
-local function AutoZepellinsHeadband()
-	while _G.ZepellinsHeadband do wait(0.01)
-		if ZepellinsHeadband then
-			ZepellinsHeadband.Parent = Character
-			wait(1)
-			local args = {
-				[1] = "EndDialogue",
-				[2] = {
-					["Option"] = "Option2",
-					["NPC"] = "Merchant",
-					["Dialogue"] = "Dialogue5"
-				}
-			}
-			game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(args))
-        end
-    end
-end
-
-local function AutoStoneMask()
-	while _G.StoneMask do wait(0.01)
-		if StoneMask then
-            StoneMask.Parent = Character
-			wait(1)
-			local args = {
-				[1] = "EndDialogue",
-				[2] = {
-					["Option"] = "Option2",
-					["NPC"] = "Merchant",
-					["Dialogue"] = "Dialogue5"
-				}
-			}
-			game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(args))
-		end
-	end
-end
-
-local function AutoQuintonsGlove()
-	while _G.QuintonsGlove do wait(0.01) 
-		if QuintonsGlove then
-			QuintonsGlove.Parent = Character
-			wait(1)
-			local args = {
-				[1] = "EndDialogue",
-				[2] = {
-					["Option"] = "Option2",
-					["NPC"] = "Merchant",
-					["Dialogue"] = "Dialogue5"
-				}
-			}
-			game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(args))
-		end
-	end
-end
-
-local function AutoSteelBall()
-	while _G.SteelBall do wait(0.01)
-		if SteelBall then
-			SteelBall.Parent = Character
-			wait(1)
-			local args = {
-				[1] = "EndDialogue",
-				[2] = {
-					["Option"] = "Option2",
-					["NPC"] = "Merchant",
-					["Dialogue"] = "Dialogue5"
-				}
-			}
-			game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(args))
-		end
-	end
-end
-
-local function AutoRibCage()
-	while _G.RibCage do wait(0.01)
-		if RibCage then
-			RibCage.Parent = Character
-			wait(1)
-			local args = {
-				[1] = "EndDialogue",
-				[2] = {
-					["Option"] = "Option2",
-					["NPC"] = "Merchant",
-					["Dialogue"] = "Dialogue5"
-				}
-			}
-			game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(args))
-		end
-	end
-end
-
-local function AutoAncientScroll()
-	while _G.AncientScroll do wait(0.01)
-		if AncientScroll then
-			AncientScroll.Parent = Character
-			wait(1)
-			local args = {
-				[1] = "EndDialogue",
-				[2] = {
-					["Option"] = "Option2",
-					["NPC"] = "Merchant",
-					["Dialogue"] = "Dialogue5"
-				}
-			}
-			game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(args))
-		end
-	end
-end
-
-local function AutoDiosDiary()
-	while _G.DiosDiary do wait(0.01)
-		if DiosDiary then
-			DiosDiary.Parent = Character
-			wait(1)
-			local args = {
-				[1] = "EndDialogue",
-				[2] = {
-					["Option"] = "Option2",
-					["NPC"] = "Merchant",
-					["Dialogue"] = "Dialogue5"
-				}
-			}
-			game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(args))
-		end
-	end
 end
 
 ---- Tab Home ----
 local Tab = Window:MakeTab({
-	Name = "Home",
-	Icon = "rbxassetid://4370345144",
-	PremiumOnly = false
+    Name = "Home",
+    Icon = "rbxassetid://4370345144",
+    PremiumOnly = false
 })
 
 ---- Tab Farming ----
 local Tab = Window:MakeTab({
-	Name = "Farming",
-	Icon = "rbxassetid://4483364237",
-	PremiumOnly = false
+    Name = "Farming",
+    Icon = "rbxassetid://4483364237",
+    PremiumOnly = false
 })
 
 ---- Tab Items ----
 local Tab = Window:MakeTab({
-	Name = "Items",
-	Icon = "rbxassetid://4335482575",
-	PremiumOnly = false
+    Name = "Items",
+    Icon = "rbxassetid://4335482575",
+    PremiumOnly = false
 })
 
 Tab:AddToggle({
-	Name = "Auto Sell Rokakaka",
-	Default = false,
-	Callback = function(Value)
-		_G.Rokakaka = Value
-		if Value then
-			AutoRokakaka()
-		end
-	end
+    Name = "Auto Sell Rokakaka",
+    Default = false,
+    Callback = function(Value)
+        _G.Rokakaka = Value
+        if Value then
+            AutoItem("Rokakaka")
+        end
+    end
 })
 
 Tab:AddToggle({
-	Name = "Auto Sell Gold Coin",
-	Default = false,
-	Callback = function(Value)
-		_G.GoldCoin = Value
-		if Value then
-			AutoGoldCoin()
-		end
-	end
+    Name = "Auto Sell Gold Coin",
+    Default = false,
+    Callback = function(Value)
+        _G.GoldCoin = Value
+        if Value then
+            AutoItem("Gold Coin")
+        end
+    end
 })
 
 Tab:AddToggle({
 	Name = "Auto Sell Myst. Arrow",
 	Default = false,
 	Callback = function(Value)
-		_G.MysteriousArrow = Value
-		if Value then
-			AutoMystArrow()
-		end
+		
 	end
 })
   
@@ -299,10 +85,7 @@ Tab:AddToggle({
 	Name = "Auto Sell Diamond",
 	Default = false,
 	Callback = function(Value)
-		_G.Diamond = Value
-		if Value then
-			AutoDiamond()
-		end
+		
 	end
 })
   
@@ -310,10 +93,7 @@ Tab:AddToggle({
 	Name = "Auto Sell Rib Cage",
 	Default = false,
 	Callback = function(Value)
-		_G.RibCage = Value
-		if Value then
-			AutoRibCage()
-		end
+		
 	end
 })
   
@@ -321,10 +101,7 @@ Tab:AddToggle({
 	Name = "Auto Sell Zepellin's Headband",
 	Default = false,
 	Callback = function(Value)
-		_G.ZepellinsHeadband = Value
-		if Value then
-			AutoZepellinsHeadband()
-		end
+		
 	end
 })
 
@@ -332,10 +109,7 @@ Tab:AddToggle({
 	Name = "Auto Sell Pure Rokakaka",
 	Default = false,
 	Callback = function(Value)
-		_G.PureRokakaka = Value
-		if Value then
-			AutoPureRokakaka()
-		end
+		
 	end
 })
   
@@ -343,10 +117,7 @@ Tab:AddToggle({
 	Name = "Stone Mask",
 	Default = false,
 	Callback = function(Value)
-		_G.StoneMask = Value
-		if Value then
-			AutoStoneMask()
-		end
+		
 	end
 })
   
@@ -354,10 +125,7 @@ Tab:AddToggle({
 	Name = "Dio's Diary",
 	Default = false,
 	Callback = function(Value)
-		_G.DiosDiary = Value
-		if Value then
-			AutoDiosDiary()
-		end
+		
 	end
 })
   
@@ -365,10 +133,7 @@ Tab:AddToggle({
 	Name = "Quinton's Glove",
 	Default = false,
 	Callback = function(Value)
-		_G.QuintonsGlove = Value
-		if Value then
-			AutoQuintonsGlove()
-		end
+		
 	end
 })
   
@@ -376,10 +141,7 @@ Tab:AddToggle({
 	Name = "Ancient Scroll",
 	Default = false,
 	Callback = function(Value)
-		_G.AncientScroll = Value
-		if Value then
-			AutoAncientScroll()
-		end
+		
 	end
 })
   
@@ -387,10 +149,7 @@ Tab:AddToggle({
 	Name = "Steel Ball",
 	Default = false,
 	Callback = function(Value)
-		_G.SteelBall = Value
-		if Value then
-			AutoSteelBall()
-		end
+		
 	end
 })
 OrionLib:Init()
