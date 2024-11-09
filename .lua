7428993ed90e34deda8fc2584b30a34309595718
Swordikk <133685730+Swordikk@users.noreply.github.com>
2024-11-09 23:01:30 +0400

    Update .lua

--- .lua
+++ .lua
@@ -34,6 +34,105 @@ local function AutoRokakaka()
     end
 end
 
+local function AutoGoldCoin()
+    while _G.GoldCoin do wait(0.01)
+        if GoldCoin then
+            GoldCoin.Parent = Character
+            wait(1)
+        end
+    end
+end
+
+local function AutoMystArrow()
+    while _G.MysteriousArrow do wait(0.01)
+        if MysteriousArrow then
+            MysteriousArrow.Parent = Character
+            wait(1)
+        end
+    end
+end
+
+local function AutoDiamond()
+    while _G.Diamond do wait(0.01)
+        if Diamond then
+            Diamond.Parent = Character
+            wait(1)
+        end
+    end
+end
+
+local function AutoPureRokakaka()
+    while _G.PureRokakaka do wait(0.01)
+        if PureRokakaka then
+            PureRokakaka.Parent = Character
+            wait(1)
+        end
+    end
+end
+
+local function AutoZepellinsHeadband()
+    while _G.ZepellinsHeadband do wait(0.01)
+        if ZepellinsHeadband then
+            ZepellinsHeadband.Parent = Character
+            wait(1)
+        end
+    end
+end
+
+local function AutoStoneMask()
+    while _G.StoneMask do wait(0.01)
+        if StoneMask then
+            StoneMask.Parent = Character
+            wait(1)
+        end
+    end
+end
+
+local function AutoQuintonsGlove()
+    while _G.QuintonsGlove do wait(0.01) 
+        if QuintonsGlove then
+            QuintonsGlove.Parent = Character
+            wait(1)
+        end
+    end
+end
+
+local function AutoSteelBall()
+    while _G.SteelBall do wait(0.01)
+        if SteelBall then
+            SteelBall.Parent = Character
+            wait(1)
+        end
+    end
+end
+
+local function AutoRibCage()
+    while _G.RibCage do wait(0.01)
+        if RibCage then
+            RibCage.Parent = Character
+            wait(1)
+        end
+    end
+end
+
+local function AutoAncientScroll()
+    while _G.AncientScroll do wait(0.01)
+        if AncientScroll then
+            AncientScroll.Parent = Character
+            wait(1)
+        end
+    end
+end
+
+local function AutoDiosDiary()
+    while _G.DiosDiary do wait(0.01)
+        if DiosDiary then
+            DiosDiary.Parent = Character
+            wait(1)
+        end
+    end
+end
+
 ---- Tab Home ----
 local Tab = Window:MakeTab({
     Name = "Home",
@@ -70,7 +169,10 @@ Tab:AddToggle({
     Name = "Auto Sell Gold Coin",
     Default = false,
     Callback = function(Value)
-        
+        _G.GoldCoin = Value
+		if Value then
+			AutoGoldCoin()
+		end
     end
 })
 
@@ -78,7 +180,10 @@ Tab:AddToggle({
 	Name = "Auto Sell Myst. Arrow",
 	Default = false,
 	Callback = function(Value)
-  
+		_G.MysteriousArrow = Value
+		if Value then
+			AutoMystArrow()
+		end
 	end
 })
   
@@ -86,7 +191,10 @@ Tab:AddToggle({
 	Name = "Auto Sell Diamond",
 	Default = false,
 	Callback = function(Value)
-  
+		_G.Diamond = Value
+		if Value then
+			AutoDiamond()
+		end
 	end
 })
   
@@ -94,7 +202,10 @@ Tab:AddToggle({
 	Name = "Auto Sell Rib Cage",
 	Default = false,
 	Callback = function(Value)
-  
+		_G.RibCage = Value
+		if Value then
+			AutoRibCage()
+		end
 	end
 })
   
@@ -102,15 +213,32 @@ Tab:AddToggle({
 	Name = "Auto Sell Zepellin's Headband",
 	Default = false,
 	Callback = function(Value)
-  
+		_G.ZepellinsHeadband = Value
+		if Value then
+			AutoZepellinsHeadband()
+		end
 	end
+})
+
+Tab:AddToggle({
+    Name = "Auto Sell Pure Rokakaka",
+    Default = false,
+    Callback = function(Value)
+        _G.PureRokakaka = Value
+        if Value then
+            AutoPureRokakaka()
+        end
+    end
 })
   
 Tab:AddToggle({
 	Name = "Stone Mask",
 	Default = false,
 	Callback = function(Value)
-  
+		_G.StoneMask = Value
+		if Value then
+			AutoStoneMask()
+		end
 	end
 })
   
@@ -118,7 +246,10 @@ Tab:AddToggle({
 	Name = "Dio's Diary",
 	Default = false,
 	Callback = function(Value)
-  
+		_G.DiosDiary = Value
+		if Value then
+			AutoDiosDiary()
+		end
 	end
 })
   
@@ -126,7 +257,10 @@ Tab:AddToggle({
 	Name = "Quinton's Glove",
 	Default = false,
 	Callback = function(Value)
-  
+		_G.QuintonsGlove = Value
+		if Value then
+			AutoQuintonsGlove()
+		end
 	end
 })
   
@@ -134,7 +268,10 @@ Tab:AddToggle({
 	Name = "Ancient Scroll",
 	Default = false,
 	Callback = function(Value)
-  
+		_G.AncientScroll = Value
+		if Value then
+			AutoAncientScroll()
+		end
 	end
 })
   
@@ -142,7 +279,10 @@ Tab:AddToggle({
 	Name = "Steel Ball",
 	Default = false,
 	Callback = function(Value)
-  
+		_G.SteelBall = Value
+		if Value then
+			AutoSteelBall()
+		end
 	end
 })
 OrionLib:Init()
