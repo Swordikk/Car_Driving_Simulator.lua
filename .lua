6cfd44bf050b6fd60002e71bbe887bf92be4125d
Swordikk <133685730+Swordikk@users.noreply.github.com>
2024-11-09 22:14:03 +0400

    Update .lua

--- .lua
+++ .lua
@@ -39,27 +39,45 @@ local function AutoRokakaka()
 				}
 			}
 			game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(args))
-        end
-    end
+		end
+	end
+end
+
+local function AutoGoldCoin()
+    while _G.GoldCoin do
+        if GoldCoin then
+            GoldCoin.Parent = Character
+            wait(1)
+			local args = {
+				[1] = "EndDialogue",
+				[2] = {
+					["Option"] = "Option2",
+					["NPC"] = "Merchant",
+					["Dialogue"] = "Dialogue5"
+				}
+			}
+			game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(args))
+		end
+	end
 end
 
 ---- Tab Home ----
-local TabHome = Window:MakeTab({
-    Name = "Home",
-    Icon = "rbxassetid://4370345144",
-    PremiumOnly = false
+local Tab = Window:MakeTab({
+	Name = "Home",
+	Icon = "rbxassetid://4370345144",
+	PremiumOnly = false
 })
 
 ---- Tab Farming ----
-local TabFarming = Window:MakeTab({
-    Name = "Farming",
-    Icon = "rbxassetid://4483364237",
-    PremiumOnly = false
+local Tab = Window:MakeTab({
+	Name = "Farming",
+	Icon = "rbxassetid://4483364237",
+	PremiumOnly = false
 })
 
 ---- Tab Items ----
-local TabItems = Window:MakeTab({
-    Name = "Items",
+local Tab = Window:MakeTab({
+	Name = "Items",
     Icon = "rbxassetid://4335482575",
     PremiumOnly = false
 })
@@ -75,12 +93,129 @@ TabItems:AddToggle({
     end
 })
 
-TabItems:AddToggle({
-    Name = "Auto Sell Gold Coin",
-    Default = false,
-    Callback = function(Value)
-        
-    end
+Tab:AddToggle({
+	Name = "Auto Sell Gold Coin",
+	Default = false,
+	Callback = function(Value)
+		_G.GoldCoin = Value
+		if Value then
+			AutoGoldCoin()
+		end
+	end
+})
+
+Tab:AddToggle({
+	Name = "Auto Sell Myst. Arrow",
+	Default = false,
+	Callback = function(Value)
+
+	end
+})
+
+Tab:AddToggle({
+	Name = "Auto Sell Diamond",
+	Default = false,
+	Callback = function(Value)
+
+	end
+})
+
+Tab:AddToggle({
+	Name = "Auto Sell Rib Cage",
+	Default = false,
+	Callback = function(Value)
+
+	end
+})
+
+Tab:AddToggle({
+	Name = "Auto Sell Zepellin's Headband",
+	Default = false,
+	Callback = function(Value)
+
+	end
+})
+
+Tab:AddToggle({
+	Name = "Stone Mask",
+	Default = false,
+	Callback = function(Value)
+
+	end
+})
+
+Tab:AddToggle({
+	Name = "Dio's Diary",
+	Default = false,
+	Callback = function(Value)
+
+	end
+})
+
+Tab:AddToggle({
+	Name = "Quinton's Glove",
+	Default = false,
+	Callback = function(Value)
+
+	end
+})
+
+Tab:AddToggle({
+	Name = "Ancient Scroll",
+	Default = false,
+	Callback = function(Value)
+
+	end
+})
+
+Tab:AddToggle({
+	Name = "Steel Ball",
+	Default = false,
+	Callback = function(Value)
+
+	end
+})
+
+---- Tab Teleports ----
+local Tab = Window:MakeTab({
+	Name = "Teleports",
+	Icon = "rbxassetid://4384402413",
+	PremiumOnly = false
+})
+
+---- Tab Misc ----
+local Tab = Window:MakeTab({
+	Name = "Misc",
+	Icon = "rbxassetid://4483362748",
+	PremiumOnly = false
+})
+
+---- Tab Themes ----
+local Tab = Window:MakeTab({
+	Name = "Themes",
+	Icon = "rbxassetid://4335483762",
+	PremiumOnly = false
+})
+
+---- Tab Visuals ----
+local Tab = Window:MakeTab({
+	Name = "Visuals",
+	Icon = "rbxassetid://3610254229",
+	PremiumOnly = false
+})
+
+---- Tab Configs ----
+local Tab = Window:MakeTab({
+	Name = "Configs",
+	Icon = "rbxassetid://3610247188",
+	PremiumOnly = false
+})
+
+---- Tab Setthing ----
+local Tab = Window:MakeTab({
+	Name = "Setthings",
+	Icon = "rbxassetid://4483345737",
+	PremiumOnly = false
 })
 
 OrionLib:Init()
