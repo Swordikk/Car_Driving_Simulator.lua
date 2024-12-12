9d7abad812f704c56914e61866740a3ba4d4ad6c
Swordikk <133685730+Swordikk@users.noreply.github.com>
2024-12-12 22:26:32 +0400

    Update .lua

--- .lua
+++ .lua
@@ -1,48 +1,75 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "Script by Swordikk | ⚡️YBA", HidePremium = false, IntroText = "Script for YBA", SaveConfig = true, IntroEnabled = true, ConfigFolder = "Scripts"})
+local Window = OrionLib:MakeWindow({Name = "Script by Swordikk | ⚡Realistik_Driving_Sinulator", HidePremium = false, IntroText = "Script for Malfoy4ik", SaveConfig = true, IntroEnabled = true, ConfigFolder = "Scripts"})
 
-local Player = game.Players.LocalPlayer
-local Character = Player.Character or Player.CharacterAdded:Wait()
-local Humanoid = Character:FindFirstChildOfClass("Humanoid")
-local HumanoidRootPart = Character:FindFirstChild("HumanoidRootPart")
+local Humanoid = game.Players.LocalPlayer.Character.Humanoid
+local HumanoidRootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
 
-local function getItem(itemName)
-    return Player.Backpack:FindFirstChild(itemName) or Player.Character:FindFirstChild(itemName)
+function AutoFarmMiles()
+
 end
 
-local function AutoItem(itemName)
-    while _G[itemName] do
-        wait(0.01)
-        local item = getItem(itemName)  -- Получаем текущий статус предмета
-        if item then
-            item.Parent = Character  -- Берем предмет в руки
-            wait(1)  -- Ожидание перед продажей или использованием
-            local args = {
-                [1] = "EndDialogue",
-                [2] = {
-                    ["Option"] = "Option2",
-                    ["NPC"] = "Merchant",
-                    ["Dialogue"] = "Dialogue5"
-                }
-            }
-            game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(args))
-        end
+function WalkSpeed()
+	while _G.WalkSpeed do game:GetService("RunService").RenderStepped:wait()
+	    Humanoid.WalkSpeed = _G.WalkSpeed
     end
 end
 
----- Tab Home ----
+function JumpPower()
+	while _G.JumpPower do game:GetService("RunService").RenderStepped:wait()
+	    Humanoid.JumpPower = _G.JumpPower
+    end
+end
+
 local Tab = Window:MakeTab({
-    Name = "Home",
-    Icon = "rbxassetid://4370345144",
-    PremiumOnly = false
+	Name = "AutoFarm",
+	Icon = "rbxassetid://4483362748",
+	PremiumOnly = false
 })
 
----- Tab Farming ----
+
+Tab:AddToggle({
+	Name = "AutoFarm Miles",
+	Default = false,
+	Callback = function(Value)
+		_G.AutoFarmMiles = Value
+		AutoFarmMiles()
+	end    
+})
+
 local Tab = Window:MakeTab({
-    Name = "Farming",
-    Icon = "rbxassetid://4483364237",
-    PremiumOnly = false
+	Name = "Misc",
+	Icon = "rbxassetid://4483362748",
+	PremiumOnly = false
+})
+
+Tab:AddTextbox({
+	Name = "Spawn Cars",
+	Default = "Cars For Cash",
+	TextDisappear = true,
+	Callback = function(Value)
+		
+	end	  
+})
+
+Tab:AddTextbox({
+	Name = "WalkSpeed",
+	Default = "",
+	TextDisappear = false,
+	Callback = function(Value)
+		_G.WalkSpeed = Value
+		WalkSpeed()
+	end
+})
+
+Tab:AddTextbox({
+	Name = "JumpPower",
+	Default = "",
+	TextDisappear = false,
+	Callback = function(Value)
+		_G.JumpPower = Value
+		JumpPower()
+	end
 })
 
 Tab:AddToggle({
@@ -69,40 +96,3 @@ Tab:AddToggle({
 		end
 	end
 })
-
----- Tab Items ----
-local Tab = Window:MakeTab({
-    Name = "Items",
-    Icon = "rbxassetid://4335482575",
-    PremiumOnly = false
-})
-
-local items = {
-    "Rokakaka",
-    "Gold Coin",
-    "Mysterious Arrow",
-    "Diamond",
-    "Rib Cage of The Saint's Corpse",
-    "Pure Rokakaka",
-    "Stone Mask",
-    "Dio's Diary",
-    "Quinton's Glove",
-    "Ancient Scroll",
-    "SteelBallRunWins",
-    "Caesar's Headband"
-}
-
-for _, itemName in ipairs(items) do
-    Tab:AddToggle({
-        Name = "Auto Sell " .. itemName,
-        Default = false,
-        Callback = function(Value)
-            _G[itemName] = Value  -- Устанавливаем глобальную переменную для этого предмета
-            if Value then
-                AutoItem(itemName)
-            end
-        end
-    })
-end
-
-OrionLib:Init()
